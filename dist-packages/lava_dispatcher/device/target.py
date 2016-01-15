cbd2628111b653cc2adcd3030cfb675a3f80acf2
Bo Wang <wangbo.hfut@outlook.com>
2016-01-15 17:48:12 +0800

    modify enter_bootloader_mstar

--- dist-packages/lava_dispatcher/device/target.py
+++ dist-packages/lava_dispatcher/device/target.py
@@ -511,14 +511,13 @@ class Target(object):
     def _enter_bootloader_mstar(self, connection):
         try:
             start = time.time()
-            connection.expect(self.config.interrupt_boot_prompt,
-                              timeout=self.config.bootloader_timeout)
-            if self.config.interrupt_boot_control_character:
-                connection.sendcontrol(self.config.interrupt_boot_control_character)
-            else:
-                for i in range(50):
-                    connection.send(self.config.interrupt_boot_command)
-                connection.expect('<< MStar >>#')
+            # connection.expect(self.config.interrupt_boot_prompt, timeout=self.config.bootloader_timeout)
+            # if self.config.interrupt_boot_control_character:
+            #     connection.sendcontrol(self.config.interrupt_boot_control_character)
+            # else:
+            for i in range(20):
+                connection.send(self.config.interrupt_boot_command)
+            connection.expect('<< MStar >>#')
             # Record the time it takes to enter the bootloader.
             enter_bootloader_time = "{0:.2f}".format(time.time() - start)
             self.context.test_data.add_result('enter_bootloader', 'pass', enter_bootloader_time, 'seconds')
