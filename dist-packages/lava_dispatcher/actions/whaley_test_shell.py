eb3c34ffe21279066720b45d14ddb8a8c3f87fb2
Bo Wang <wangbo.hfut@outlook.com>
2016-01-29 17:11:32 +0800

    add whaley_test_shell command

---
+++ dist-packages/lava_dispatcher/actions/whaley_test_shell.py
@@ -0,0 +1,39 @@
+#!/usr/bin/python
+
+# Author: Bo Wang <wang.bo@whaley.cn>
+# Date: 2016.01.29
+
+import logging
+import stat
+import os
+
+from lava_dispatcher.actions import BaseAction
+from lava_dispatcher.utils import finalize_process
+
+# 755 file permissions
+XMOD = stat.S_IRWXU | stat.S_IXGRP | stat.S_IRGRP | stat.S_IXOTH | stat.S_IROTH
+
+class cmd_whaley_test_shell(BaseAction):
+
+    parameters_schema = {
+        'type': 'object',
+        'properties': {
+            'script': {'type': 'string', 'optional': False}
+        },
+        'additionalProperties': False,
+    }
+
+    def __init__(self, context):
+        super(cmd_whaley_test_shell, self).__init__(context)
+
+    def run(self, script=None):
+        target = self.client.target_device
+        proc = target.proc
+        logging.warning("Disconnect the serial connection, try to run the script")
+        if proc:
+            finalize_process(proc)
+            self.client.target_device.proc = None
+        # add 755 file permissions
+        os.chmod(script, XMOD)
+        logging.info("Run command in file: %s", script)
+        self.context.run_command_get_output(script)
