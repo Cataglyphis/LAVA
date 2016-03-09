cddbb9cec06df933a599d03cc6c2aafb6605d209
Bo <wangbo.hfut@outlook.com>
2016-03-09 16:04:29 +0800

    modify np 1601.py, change timeout

--- pdudaemon/lavapdu/drivers/np1601.py
+++ pdudaemon/lavapdu/drivers/np1601.py
@@ -23,7 +23,7 @@ class NP1601(object):
     def _get_connection(self):
         logging.debug("Connection to NP1601 PDU with: %s" % self.exec_string)
         self.connection = pexpect.spawn(self.exec_string)
-        self.connection.expect("Synaccess", timeout=10)
+        self.connection.expect("Synaccess", timeout=3)
         username = self.pdus[self.hostname]["username"]
         password = self.pdus[self.hostname]["password"]
         self._pdu_login(username, password)
